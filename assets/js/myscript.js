53b0708bd2424eca26f48ada78dd57885126a571
mati <jmsalinas@uc.cl>
2019-01-16 21:45:26 -0300

    front-page

--- assets/js/myscript.js
+++ assets/js/myscript.js
@@ -1,3 +1,44 @@
+	// NAVIGATION MENU AND SECTION SNAP
+
+	function scrollToClass(aid) {
+		var aTag = $('' + aid)
+		$('html, body').animate({ scrollTop: aTag.offset().top }, 'slow')
+	}
+
+	$(".menu ul li a").on('click', function() {	
+		var target = $(this).attr('href')
+		scrollToClass(target)
+	})
+
+	$(function(){
+		$( '.snap' ).scrollSnap()
+	})
+
+	// MENU
+
+	var menuShow = $('.menu__responsive').children(".menu__responsive--hidden").hasClass('show');
+
+	function desplazar (){
+		$('.menu__responsive--hidden').toggleClass('show');
+		$('#hamburger').toggleClass('hamburguer-slide');
+	}
+
+	$('#hamburger').on('click', function(e){		
+		desplazar();
+		e.stopPropagation();
+		menuShow = $('.menu__responsive').children(".menu__responsive--hidden").hasClass('show');
+		console.log(menuShow)
+	})
+   
+	window.addEventListener('click', function(e){
+		// var menuHide = $('.menu-hide');
+		if (menuShow === true && e.target.className != 'menu__responsive--hidden show') {
+			desplazar();			
+		}		
+		console.log(e.target)
+	})
+
+
 	// FlexSlider 
 	
 	$(window).load(function(){
@@ -8,3 +49,6 @@
 	    }
 	  });
 	});
+
+
+	
